b2df65471f3e1bb846996256da78b4f3bef0badf
Sean Dela Cruz <sean.delacruz@gmail.com>
2015-10-26 21:27:44 -0700

    declared classes

---
+++ Game.java
@@ -0,0 +1,7 @@
+public class Game {
+	private Board chessboard;
+
+	public Game() {
+		chessboard = new Board();
+	}
+}
---
+++ potatochess.java
@@ -0,0 +1,5 @@
+public class potatochess {
+	public static void main(String[] args) {
+		Game game = new Game();
+	}
+}
